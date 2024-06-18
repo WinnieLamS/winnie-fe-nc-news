413d2ee2b1643bee4d1f125e11a81c6cfc7f4a6f
WinnieLam <winnie.lin.1225@gmail.com>
2024-06-18 08:46:23 +0100

    complete list articles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,29 +1,41 @@
 import { useState, useEffect } from "react";
 import { getArticles, getArticleById } from "../Api";
 import { useNavigate } from "react-router-dom";
+import  loadingPink  from "../images/loadingPink.gif";
 
 export const Home = ({setArticle}) => {
     const navigate = useNavigate();
     const [articles, setArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     
     
 
     useEffect(() => {
+        setIsLoading(true)
         getArticles().then((articlesArr) => {
           setArticles(articlesArr.slice(0, 10));
+          setIsLoading(false)
         });
     }, []);
 
     const handleArticleClick = (article_id) => {
+        setIsLoading(true)
         getArticleById(article_id)
             .then((articleFromApi) => {
                 setArticle(articleFromApi);
                 navigate(`/articles/${articleFromApi.title}`); 
+                setIsLoading(false)
+
             })
     };
 
-    return (
-        <>
+    return isLoading?(
+        <div className="loading-container">
+            <h2>Loading......</h2>
+            <img src={loadingPink} alt="Loading..." />
+        </div>
+    ):(
+        <div>
             <h2>The Ten Latest News:</h2>
                 {articles.map((article) => {
                     return (
@@ -34,6 +46,6 @@ export const Home = ({setArticle}) => {
                     </div>
                     )
                 })}
-        </>
+        </div>
     );
 }
