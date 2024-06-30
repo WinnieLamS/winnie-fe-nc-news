15746e26865c2dfdfb34f8f2a276ba4a40abfe52
WinnieLam <winnie.lin.1225@gmail.com>
2024-06-30 17:09:40 +0100

    Fix Rollup error by marking greyHome.png as external in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        '/Users/winnielin/winnie-nc-news/src/images/greyHome.png'
+      ]
+    }
+  }
+});
